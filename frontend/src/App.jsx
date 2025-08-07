eaa13712d1ba75bb5d17d4d73fad73d6c4fdb2f3
Abhi-576 <abhinavsharma9473@gmail.com>
2025-08-07 16:34:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Login from './login_ui/Login.jsx';
-import SignUP from './login_ui/SignUp.jsx';
-
+//import SignUP from '/SignUp.jsx';
+import SignUp from './login_ui/SignUp_no_ai_fr_fr'
+import Products from ./Products_page/Product.jsx
 //import './App.css'
 
 const App = () => {
   return (
     <div>
       <Login />
+      <SignUp />
+      <Products />
     </div>
   );
 };
