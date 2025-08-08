c0f992c1493852cb2571fff1a014da8ffbd0f1a2
Abhi-576 <abhinavsharma9473@gmail.com>
2025-08-08 18:55:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './login_ui/Login.jsx';
 import SignUp from './login_ui/SignUp(no_ai_fr_fr).jsx'
 import Products from './Products/Products_page.jsx'
 import ProductInfo from './Products/productInfo.jsx';
+import UserAccountPage from './profile_page/user_profile.jsx';
 import "./index.css"
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
         <Route path='/' element={<Products/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/product/:id' element={<ProductInfo/>}/>
+          <Route path='/profile' element={<UserAccountPage/>}/>
       </Routes>
     </BrowserRouter>
   );
