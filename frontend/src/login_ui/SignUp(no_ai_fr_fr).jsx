953192d2969f8691c4d52cb3ce5491fedd54c586
Abhi-576 <abhinavsharma9473@gmail.com>
2025-08-08 21:18:12 +0530

    Update SignUp(no_ai_fr_fr).jsx

--- frontend/src/login_ui/SignUp(no_ai_fr_fr).jsx
+++ frontend/src/login_ui/SignUp(no_ai_fr_fr).jsx
@@ -3,13 +3,16 @@ import {useNavigate} from 'react-router-dom'
 
 const SignUp = () => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');                       //added email
   const [password, setPassword] = useState('');
   const [responseMsg , setresponseMsg] = useState(null) ;
   const navigate = useNavigate() ;
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const data = {
       username : username ,
+      email : email ,                                           //added email
       password : password
     }
     const response = await fetch("http://localhost:3001/user/signup" , {
@@ -22,6 +25,7 @@ const SignUp = () => {
     const responsedata = await response.json() ;
     if (responsedata){
       setUsername('') ;
+      setEmail('') ;                                            //added email
       setPassword('') ;
       navigate('/login')
     }
@@ -29,8 +33,8 @@ const SignUp = () => {
   };
 return (
     <>
-    
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 h-full bg-gray-900">
+    <div className='h-screen w-full bg-gray-900'>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
@@ -51,6 +55,24 @@ return (
                   required
                   onChange={(e) =>{
                     setUsername(e.target.value)
+                  }}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100 text-left">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  value={email}                                             //added email
+                  type='email'
+                  required
+                  onChange={(e) =>{
+                    setEmail(e.target.value)
                   }}
                   className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
@@ -85,7 +107,7 @@ return (
 
             <div>
               <button
-                onClick={handleSubmit} 
+                onClick={handleSubmit}             
                 className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold mt-4 text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Sign in
@@ -105,7 +127,7 @@ return (
 
         </div>
       </div>
-      
+      </div>
     </>
   )
 };
